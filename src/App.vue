c1d06b570fb3c59ed6204821d475835994b419d3
BradleyNotFound <148509036+BradleyNotFound@users.noreply.github.com>
2025-07-10 16:04:29 +0800

    Add particle background component and integrate

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
+    <ParticleBackground />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-    export default {
-        name: "app",
-        mounted() {
-            //只要刷新页面，就会重新加载路由树，保证了路由不会丢失数据
-            if (localStorage.getItem("userInfo")) {
-                this.$store.dispatch("app/setMenuList");
-            }
-        },
-    };
+import ParticleBackground from "./components/ParticleBackground.vue";
+
+export default {
+  name: "app",
+  components: { ParticleBackground },
+  mounted() {
+    //只要刷新页面，就会重新加载路由树，保证了路由不会丢失数据
+    if (localStorage.getItem("userInfo")) {
+      this.$store.dispatch("app/setMenuList");
+    }
+  },
+};
 </script>
