db818888e9da37ce35ae0c1a29cb8277ca350837
Ethan_Brooks <2580733979@qq.com>
2025-07-10 15:10:42 +0800

    增加： AiChatvue AI_CHAT_REDEME 修改： scr/App.vue .env.developmet .env.production package-lock.json

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
     <router-view></router-view>
+    <!-- AI聊天助手 -->
+    <ai-chat></ai-chat>
   </div>
 </template>
 
 <script>
-    export default {
-        name: "app",
-        mounted() {
-            //只要刷新页面，就会重新加载路由树，保证了路由不会丢失数据
-            if (localStorage.getItem("userInfo")) {
-                this.$store.dispatch("app/setMenuList");
-            }
-        },
-    };
+import AiChat from './components/AiChat.vue'
+
+export default {
+    name: "app",
+    components: {
+        AiChat
+    },
+    mounted() {
+        //只要刷新页面，就会重新加载路由树，保证了路由不会丢失数据
+        if (localStorage.getItem("userInfo")) {
+            this.$store.dispatch("app/setMenuList");
+        }
+    },
+};
 </script>
